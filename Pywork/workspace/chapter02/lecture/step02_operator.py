136e6210c6d9f8640ef306dfd6074dd49cc849a5
Kyle Kwak <prophet@hanyang.ac.kr>
2023-01-08 00:42:11 +0900

    chapter02

---
+++ Pywork/workspace/chapter02/lecture/step02_operator.py
@@ -0,0 +1,85 @@
+# 산술 연산자
+num1 = 100  # 피연산자1
+num2 = 30  # 피연산자2
+print('num1 =', num1)
+print('num2 =', num2)
+
+print('=' * 100)
+
+add = num1 + num2  # 덧셈
+print('add =', add)
+sub = num1 - num2  # 뺄셈
+print('sub =', sub)
+mul = num1 * num2  # 곱셈
+print('mul =', mul)
+div = num1 / num2  # 나눗셈
+print('div =', div)
+div2 = num1 // num2  # 몫
+print('div2 =', div2)
+div3 = num1 % num2  # 나머지
+print('div3 =', div3)
+square = num1 ** 2  # 제곱
+print('square =', square)
+
+print('=' * 100)
+
+# 관계 연산자
+# (1) 동등 비교
+bool_result = num1 == num2  # 두 변수의 값이 같은지 비교
+print(bool_result)
+bool_result = num1 != num2  # 두 변수의 값이 다른지 비교
+print(bool_result)
+
+# (2) 크기 비교
+bool_result = num1 > num2  # num1 값이 큰지 비교
+print(bool_result)
+bool_result = num1 >= num2  # num1 값이 크거나 같은지 비교
+print(bool_result)
+bool_result = num1 < num2  # num2 값이 큰지 비교
+print(bool_result)
+bool_result = num1 <= num2  # num2 값이 크거나 같은지 비교
+print(bool_result)
+
+print('=' * 100)
+
+# 논리 연산자
+# 두 관계식이 같은지 판단
+log_result = num1 >= 50 and num2 <= 10
+print(log_result)
+
+# 두 관계식 중 하나라도 같은지 판단
+log_result = num1 >= 50 or num2 <= 10
+print(log_result)
+
+log_result = num1 >= 50  # 관계식 판단
+print(log_result)
+
+# 괄호 안의 관계식 판단 결과에 대한 부정
+log_result = not (num1 >= 50)
+print(log_result)
+
+print('=' * 100)
+
+# 대입 연산자
+# (1) 변수에 값 할당(=)
+i = tot = 10  # i = 10; tot = 10
+i += 1  # i = i + 1
+tot += i  # tot = tot + i
+print('i = %d, tot = %d' % (i, tot))
+
+# 같은 줄에 중복 출력
+print('출력1', end=', ')  # end='구분자'
+print('출력2')
+
+# (2) 변수 교체
+v1, v2 = 100, 200
+v2, v1 = v1, v2
+print('v1 = %d, v2 = %d' % (v1, v2))
+
+# (3) 패킹(Packing) 할당
+lst = [1, 2, 3, 4, 5]
+v1, *v2 = lst
+print('v1 = {}, v2 = {}'.format(v1, v2))
+
+*v1, v2 = lst
+print('v1 = {}, v2 = {}'.format(v1, v2))
---
+++ Pywork/workspace/chapter02/lecture/step03_std_input_output.py
@@ -0,0 +1,48 @@
+# # 표준 입력장치
+# # (1) 문자형 숫자 입력
+# num = input("숫자 입력: ")
+# print('num type:', type(num))  # <class 'str'>
+# print('num =', num)
+# print('num * 2 =', num * 2)  # 100100 → 문자열이므로 2회 반복
+#
+# # (2) 문자형 숫자를 정수형으로 변환
+# num1 = int(input("숫자 입력: "))  # str → int (형변환)
+# print('num1 * 2 =', num1 * 2)
+#
+# # (3) 문자형 숫자를 실수형으로 변환
+# num2 = float(input("숫자 입력: "))  # str → float (형변환)
+# result = num1 + num2  # 실수 = 정수 + 실수
+# print('result =', result)
+
+# 표준 출력장치
+# (1) value 인수
+print('value =', 10 + 20 + 30 + 40 + 50)
+
+# (2) sep 인수: 값과 값을 특수문자로 구분
+print('010', '1234', '5678', sep='-')
+
+# (3) end 인수
+print('value =', 10, end=', ')
+print('value =', 20)
+
+# format과 양식문자
+# (4) format() 함수 인수: format(value, 'format')
+print('원주율 =', format(3.1415926535, '8.3f'))
+print('금액 =', format(10000, '10d'))
+print('금액 =', format(125000, '10,d'))
+
+# (5) 양식문자 인수: print('%양식문자' % (값))
+name = '홍길동'
+age = 35
+price = 125.456
+print('이름: %s, 나이: %d, data = %.2f' % (name, age, price))
+
+# 외부상수 출력
+# (6) 외부 상수 인수
+print('이름: {}, 나이: {}, data = {}'.format(name, age, price))
+print('이름: {1}, 나이: {0}, data = {2}'.format(age, name, price))
+
+# (7) format 축약형(SQL문 작성)
+uid = input('uid input: ')
+query = f'SELECT * FROM MEMBER WHERE UID = {uid}'
+print(query)    # MEMBER 테이블에서 uid가 hong인 레코드 검색
