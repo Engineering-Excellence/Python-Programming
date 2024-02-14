289f709bcd4da35bcdd879f29a8e9dfccbba6e7a
Kyle <prophet@hanyang.ac.kr>
2024-02-15 02:44:13 +0900

    연산자   - 관계 연산자

--- Pywork/chapter02/lecture/step02_operator.py
+++ Pywork/chapter02/lecture/step02_operator.py
@@ -24,3 +24,22 @@ square = num1 ** 2  # 제곱
 print('square =', square)
 
 print('=' * 100)
+
+# 2.2.2 관계 연산자
+# (1) 동등 비교
+bool_result = num1 == num2  # 두 변수의 값이 같은지 비교
+print(bool_result)
+bool_result = num1 != num2  # 두 변수의 값이 다른지 비교
+print(bool_result)
+
+# (2) 크기 비교
+bool_result = num1 > num2  # num1 값이 큰지 비교
+print(bool_result)
+bool_result = num1 >= num2  # num1 값이 크거나 같은지 비교
+print(bool_result)
+bool_result = num1 < num2  # num2 값이 큰지 비교
+print(bool_result)
+bool_result = num1 <= num2  # num2 값이 크거나 같은지 비교
+print(bool_result)
+
+print('=' * 100)
