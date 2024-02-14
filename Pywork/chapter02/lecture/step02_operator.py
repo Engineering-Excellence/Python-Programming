42e1ce1f36cd2630cfd73e9ea2cd1b2a5004901f
Kyle <prophet@hanyang.ac.kr>
2024-02-15 02:43:13 +0900

    연산자   - 산술 연산자

---
+++ Pywork/chapter02/lecture/step02_operator.py
@@ -0,0 +1,26 @@
+# 2.2 연산자(Operator)
+
+# 2.2.1 산술 연산자
+num1 = 100  # 피연산자1
+num2 = 30  # 피연산자2
+print('num1 =', num1)
+print('num2 =', num2)
+
+print('=' * 100)
+
+add = num1 + num2  # 덧셈
+print('add =', add)
+sub = num1 - num2  # 뺄셈
+print('sub =', sub)
+mul = num1 * num2  # 곱셈
+print('mul =', mul)
+div = num1 / num2  # 나눗셈
+print('div =', div)
+div2 = num1 // num2  # 몫
+print('div2 =', div2)
+div3 = num1 % num2  # 나머지
+print('div3 =', div3)
+square = num1 ** 2  # 제곱
+print('square =', square)
+
+print('=' * 100)
