ddaae3eff7214060d8915ec3005548372ccd48b1
Kyle <prophet@hanyang.ac.kr>
2024-02-15 02:44:42 +0900

    연산자   - 논리 연산자

--- Pywork/chapter02/lecture/step02_operator.py
+++ Pywork/chapter02/lecture/step02_operator.py
@@ -43,3 +43,21 @@ bool_result = num1 <= num2  # num2 값이 크거나 같은지 비교
 print(bool_result)
 
 print('=' * 100)
+
+# 2.2.3 논리 연산자
+# 두 관계식이 같은지 판단
+log_result = num1 >= 50 and num2 <= 10
+print(log_result)
+
+# 두 관계식 중 하나라도 같은지 판단
+log_result = num1 >= 50 or num2 <= 10
+print(log_result)
+
+log_result = num1 >= 50  # 관계식 판단
+print(log_result)
+
+# 괄호 안의 관계식 판단 결과에 대한 부정
+log_result = not (num1 >= 50)
+print(log_result)
+
+print('=' * 100)
