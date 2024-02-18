0233e1341712896b4fdd40bb24c1b6d7a6028a91
Kyle <prophet@hanyang.ac.kr>
2024-02-18 11:47:22 +0900

    문자열(String)   - 문자열 처리 함수

--- Pywork/chapter02/lecture/step04_string.py
+++ Pywork/chapter02/lecture/step04_string.py
@@ -56,3 +56,28 @@ print('-' * 100)
 # (3) 부분 문자열 생성
 subString = oneLine[-11:]
 print(subString)
+
+print('=' * 100)
+
+# 2.4.3 문자열 처리 함수
+# (1) 특정 글자 수 반환
+print('t 글자 수:', oneLine.count('t'))
+
+# (2) 접두어 문자 비교 판단
+print(oneLine.startswith('this'))
+print(oneLine.startswith('that'))
+
+# (3) 문자열 교체
+print(oneLine.replace('this', 'that'))
+
+# (4) 문자열 분리(split): 문단 → 문장
+sent = multiLine.split('\n')
+print('문장:', sent)
+
+# (5) 문자열 분리(split)2 문장 → 단어
+words = oneLine.split(' ')  # split(sep=' '): default
+print('단어:', words)
+
+# (6) 문자열 결합(join): 단어 → 문장
+sent2 = ', '.join(words)  # '구분자'.join(string)
+print(sent2)  # this, is, one, line, string
