7587553f649b8d17dfb2b2eb44e7850f93d595a1
Kyle <prophet@hanyang.ac.kr>
2024-02-18 11:44:52 +0900

    문자열(String)   - 문자열 특징

--- Pywork/chapter02/lecture/step04_string.py
+++ Pywork/chapter02/lecture/step04_string.py
@@ -17,3 +17,11 @@ multiLine2 = 'this is\nmulti line\nstring too'
 print(multiLine2)
 
 print('=' * 100)
+
+# 2.4.1 문자열 특징
+# (1) 문자열 색인
+string = 'PYTHON'
+print(string[0])
+print(string[5])
+print(string[-1])
+print(string[-6])
