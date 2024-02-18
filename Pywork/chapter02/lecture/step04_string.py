8fd40edfe8b20c41ed4593dbabe1220d47375454
Kyle <prophet@hanyang.ac.kr>
2024-02-18 11:43:51 +0900

    문자열(String)   - 문자열 유형

---
+++ Pywork/chapter02/lecture/step04_string.py
@@ -0,0 +1,19 @@
+# 2.4 문자열(String)
+
+# 문자열 유형
+oneLine = 'this is one line string'
+print(oneLine)
+
+print('-' * 100)
+
+multiLine = '''this is
+multi line
+string'''
+print(multiLine)
+
+print('-' * 100)
+
+multiLine2 = 'this is\nmulti line\nstring too'
+print(multiLine2)
+
+print('=' * 100)
