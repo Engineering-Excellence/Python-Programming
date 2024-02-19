bb6d743cdd566f91b19d99d1e3b9b2cfad313a47
Kyle <prophet@hanyang.ac.kr>
2024-02-20 00:21:11 +0900

    조건문   - 삼항 조건문

--- Pywork/chapter03/lecture/step01_if.py
+++ Pywork/chapter03/lecture/step01_if.py
@@ -30,3 +30,19 @@ else:
     grade = '저조'
 
 print('당신의 점수는 %d이고, 등급은 %s' % (score, grade))
+
+# 3.2.3 삼항 조건문
+# (1) 일반 조건문
+result = 0
+num = 9  # 초기화
+
+if num >= 5:
+    result = num * 2
+else:
+    result = num + 2
+print('result =', result)
+
+# (2) 3항 연산자
+# 변수 = 참 if (조건문) else 거짓
+result2 = num * 2 if num >= 5 else num + 2
+print('result2 =', result2)
