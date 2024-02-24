4104fc9d2fba3d64651e792a92e854fa95365d4c
Kyle <prophet@hanyang.ac.kr>
2024-02-25 00:38:22 +0900

    반복문   - for & range

--- Pywork/chapter03/lecture/step03_for.py
+++ Pywork/chapter03/lecture/step03_for.py
@@ -12,3 +12,25 @@ lstset = [1, 2, 3, 4, 5]  # 5개 원소를 갖는 열거형객체
 
 for e in lstset:
     print('원소:', e)
+
+print()
+
+# 3.3.5 for & range
+# (1) range 객체 생성
+num1 = range(10)  # range(start)
+print('num1 =', num1)
+num2 = range(1, 10)  # range(start, stop)
+print('num2 =', num2)
+num3 = range(1, 10, 2)  # range(start, stop, step)
+print('num3 =', num3)
+
+# (2) range 객체 활용
+for n in num1:
+    print(n, end=' ')
+print()
+for n in num2:
+    print(n, end=' ')
+print()
+for n in num3:
+    print(n, end=' ')
+print()
