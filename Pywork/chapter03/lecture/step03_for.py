9d8830303ecd24d986fb3065ee1549b552f21488
Kyle <prophet@hanyang.ac.kr>
2024-02-26 00:16:45 +0900

    반복문   - for & list

--- Pywork/chapter03/lecture/step03_for.py
+++ Pywork/chapter03/lecture/step03_for.py
@@ -34,3 +34,19 @@ print()
 for n in num3:
     print(n, end=' ')
 print()
+
+print()
+
+# 3.3.6 for & list
+# (1) list에 자료 저장하기
+import random
+lst = []  # 빈 list 만들기
+for i in range(10):  # 0~9
+    r = random.randint(1, 10)  # 난수 발생
+    lst.append(r)  # 난수 저장
+
+print('lst =', lst)  # 난수 출력
+
+# (2) list에 자료 참조하기
+for i in range(10):
+    print(lst[i] * 0.25)  # 난수 * 0.25
