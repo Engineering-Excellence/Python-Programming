251ec1a09ba7385491c2aaae91031d1f695093c8
Kyle <prophet@hanyang.ac.kr>
2024-02-27 00:48:49 +0900

    반복문   - 중첩 반복문

--- Pywork/chapter03/lecture/step03_for.py
+++ Pywork/chapter03/lecture/step03_for.py
@@ -50,3 +50,36 @@ print('lst =', lst)  # 난수 출력
 # (2) list에 자료 참조하기
 for i in range(10):
     print(lst[i] * 0.25)  # 난수 * 0.25
+
+print()
+
+# 3.3.7 중첩 반복문
+# 구구단 출력: range() 함수 이용
+# (1) 바깥쪽 반복문
+for i in range(2, 10):
+    print('~~~ {}단 ~~~'.format(i))
+
+    # (2) 안쪽 반복문
+    for j in range(1, 10):
+        print('%d * %d = %d' % (i, j, i * j))
+    print()
+
+# 문장과 단어 추출
+string = """나는 홍길동입니다.
+주소는 서울시입니다.
+나이는 99세입니다."""
+
+sents = []  # 문장 저장
+words = []  # 단어 저장
+
+# (1) 문단 -> 문장
+for sen in string.split(sep="\n"):
+    sents.append(sen)
+    # (2) 문장 -> 단어
+    for word in sen.split():
+        words.append(word)
+
+print('문장:', sents)
+print('문장 수:', len(sents))
+print('단어:', words)
+print('단어 수:', len(words))
