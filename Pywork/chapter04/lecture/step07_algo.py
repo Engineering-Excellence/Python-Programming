f1242acd5f4bcec05591fc3a92a3595edadbabbf
Kyle <propheta@yonsei.ac.kr>
2024-05-24 00:01:49 +0900

    알고리즘(Algorithm)   - 정렬(sort)

--- Pywork/chapter04/lecture/step07_algo.py
+++ Pywork/chapter04/lecture/step07_algo.py
@@ -24,3 +24,28 @@ for i in dataset:
 
 # (4) 결과 출력
 print(f'max = {vmax}, min = {vmin}')
+
+print()
+
+# 4.5.2 정렬(sort)
+# 선택 정렬: 첫 번째 원소를 기준으로 나머지 모든 원소를 비교하여 정렬
+# (1) 오름차순 정렬
+dataset = [3, 5, 1, 2, 4]
+n = len(dataset)
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        if dataset[i] > dataset[j]:
+            dataset[i], dataset[j] = dataset[j], dataset[i]
+    print(dataset)  # 각 회전 정렬 내용
+print(dataset)  # [1, 2, 3, 4, 5]
+
+print()
+
+# (2) 내림차순 정렬
+dataset = [3, 5, 1, 2, 4]
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        if dataset[i] < dataset[j]:
+            dataset[i], dataset[j] = dataset[j], dataset[i]
+    print(dataset)
+print(dataset)
