fb7c4d2da1f01dd4cdbddad7a63cc97fa2d7f5b3
Kyle <propheta@yonsei.ac.kr>
2024-05-25 01:13:31 +0900

    알고리즘(Algorithm)   - 검색(search)

--- Pywork/chapter04/lecture/step07_algo.py
+++ Pywork/chapter04/lecture/step07_algo.py
@@ -49,3 +49,32 @@ for i in range(n - 1):
             dataset[i], dataset[j] = dataset[j], dataset[i]
     print(dataset)
 print(dataset)
+
+print()
+
+# 4.5.3 검색(Search)
+# 이진검색(Binary Search): 전체 원소가 정렬된 상태에서 중앙 위치(mid)를 계산하여 절반은 버리고, 나머지 절반을 대상으로 검색을 수행
+dataset = [5, 10, 18, 22, 35, 55, 75, 103]
+value = int(input("검색할 값 입력: "))
+
+low = 0  # start 위치
+high = len(dataset) - 1  # end 위치
+loc = 0
+state = False  # 상태 변수
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if dataset[mid] > value:  # 중앙값이 큰 경우
+        high = mid - 1
+    elif dataset[mid] < value:  # 중앙값이 작은 경우
+        low = mid + 1
+    else:  # 찾은 경우
+        loc = mid
+        state = True
+        break  # 반복 exit
+
+if state:
+    print('찾은 위치: %d번째' % (loc + 1))
+else:
+    print('찾는 값은 없습니다.')
