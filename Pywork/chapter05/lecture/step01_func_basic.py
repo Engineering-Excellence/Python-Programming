13d2be937e2a7cb6aa54284ac5b22138294a0d02
Kyle <propheta@yonsei.ac.kr>
2024-05-26 02:56:10 +0900

    사용자정의함수   - 피타고라스 정리

--- Pywork/chapter05/lecture/step01_func_basic.py
+++ Pywork/chapter05/lecture/step01_func_basic.py
@@ -30,16 +30,18 @@ def userFunc1():
     print('인수가 없는 함수')
     print('userFunc1')
 
+
 userFunc1()  # 함수 호출
 
 print()
 
 
 # (2) 인수가 있는 함수
-def userFunc2(x, y):
+def userFunc2(x: int, y: int) -> int:
     print('userFunc2')
     z = x + y
     print('z =', z)
+
 
 userFunc2(10, 20)  # 함수 호출
 
@@ -47,13 +49,14 @@ print()
 
 
 # (3) return 있는 함수
-def userFunc3(x, y):
+def userFunc3(x: int, y: int) -> tuple:
     print('userFunc3')
     tot = x + y
     sub = x - y
     mul = x * y
     div = x / y
     return tot, sub, mul, div
+
 
 # 실인수: 키보드 입력
 x = int(input('x 입력: '))
--- Pywork/chapter05/lecture/step02_func_app.py
+++ Pywork/chapter05/lecture/step02_func_app.py
@@ -6,16 +6,18 @@ dataset = [2, 4, 5, 6, 1, 8]
 
 
 # (1) 산술평균
-def Avg(data):
+def Avg(data: list) -> float:
     avg = mean(data)
     return avg
 
 
 print("산술평균 =", Avg(dataset))
 
+print()
+
 
 # (2) 분산/표준편차
-def var_sd(data):
+def var_sd(data: list) -> tuple:
     avg = Avg(data)  # 함수 호출
 
     # list 내포
@@ -29,5 +31,19 @@ def var_sd(data):
 
 # (3) 함수 호출
 v, s = var_sd(dataset)
-print("분산 =", v)
+print("분산(사용자정의함수) =", v)
+print("분산(내장함수) =", variance(dataset))
 print("표준편차 =", s)
+
+print()
+
+
+# 5.4.2 피타고라스 정리
+def pytha(s: int, t: int):
+    a = s ** 2 - t ** 2
+    b = 2 * s * t
+    c = s ** 2 + t ** 2
+    print(f'3변의 길이: {a}, {b}, {c}')
+
+
+pytha(2, 1)  # s, t의 인수는 양의 정수를 갖는다.
