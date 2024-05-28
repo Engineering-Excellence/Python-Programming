bc3be347ba1c0221c7980e8839b6afe865ba2235
Kyle <propheta@yonsei.ac.kr>
2024-05-29 00:30:55 +0900

    중첩함수   - 중첩함수 역할

--- Pywork/chapter05/lecture/step04_inner_func.py
+++ Pywork/chapter05/lecture/step04_inner_func.py
@@ -41,3 +41,48 @@ tot_val = tot()
 print('tot =', tot_val)
 avg_val = avg(tot_val)
 print('avg =', avg_val)
+
+print()
+
+# 5.6.2 중첩함수 역할
+# 외부 함수: 함수에서 사용할 자료를 만들고, 내부 함수를 포함하는 역할을 한다.
+# 내부 함수: 외부 함수에서 만든 자료를 연산하고 조작하는 역할을 한다.
+
+from statistics import mean
+from math import sqrt
+
+data = [4, 5, 3.5, 2.5, 6.3, 5.5]
+
+
+# (1) 외부 함수: 산포도 함수
+def scattering_func(data: list) -> tuple:  # outer
+    dataSet = data  # data 생성
+
+    # (2) 내부 함수: 산술평균 반환
+    def avg_func():
+        avg_val = mean(dataSet)
+        return avg_val
+
+    # (3) 내부 함수: 분산 반환
+    def var_func(avg):
+        diff = [(data - avg) ** 2 for data in dataSet]
+        # print(sum(diff))  # 차의 합
+        var_val = sum(diff) / (len(dataSet) - 1)
+        return var_val
+
+    # (4) 내부 함수: 표준편차 반환
+    def std_func(var):
+        std_val = sqrt(var)
+        return std_val
+
+    # 함수 클로저 반환
+    return avg_func, var_func, std_func
+
+
+# (5) 외부 함수 호출
+avg, var, std = scattering_func(data)
+
+# (6) 내부 함수 호출
+print('평균:', avg())
+print('분산:', var(avg()))
+print('표준편차:', std(var(avg())))
