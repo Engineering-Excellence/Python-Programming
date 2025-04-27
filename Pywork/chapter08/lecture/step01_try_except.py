38e60ad249249d7a955b42f3cd28abea6045fbde
Kyle <propheta@yonsei.ac.kr>
2025-04-28 00:08:25 +0900

    예외처리   - 다중 예외처리

--- Pywork/chapter08/lecture/step01_try_except.py
+++ Pywork/chapter08/lecture/step01_try_except.py
@@ -23,3 +23,25 @@ for i in x:
     except:  # exception 블록
         print('숫자 아님: %s' % i)
 print('프로그램 종료')
+
+# 8.1.2 다중 예외처리
+
+# 유형별 예외처리
+print('\n유형별 예외처리')
+try:
+    div: float = 1000 / 2.53
+    print('div = %5.2f' % div)  # 정상
+    div = 1000 / 0  # 1차: 산술적 예외
+    f: TextIOWrapper = open(file=f'{sep}test.txt')  # 2차: 파일 열기 예외
+    num: int = int(input('숫자 입력: '))  # 3차: 기타 예외
+    print('num =', num)
+
+# 다중 예외처리 클래스
+except ZeroDivisionError as e:  # 산술적 예외처리
+    print('오류 정보:', e)
+except FileNotFoundError as e:  # 파일 열기 예외처리
+    print('오류 정보:', e)
+except Exception as e:  # 기타 예외처리
+    print('오류 정보:', e)
+finally:
+    print('finally 영역 - 항상 실행되는 영역')
