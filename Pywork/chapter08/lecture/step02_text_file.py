c403a7bdb018df0f6bdbd98cf2178250a568639d
Kyle <propheta@yonsei.ac.kr>
2025-04-30 00:56:04 +0900

    텍스트 파일   - 텍스트 자료 읽기

--- Pywork/chapter08/lecture/step02_text_file.py
+++ Pywork/chapter08/lecture/step02_text_file.py
@@ -31,3 +31,41 @@ finally:
     ftest1.close()  # 파일 객체 닫기
     ftest2.close()
     ftest3.close()
+
+print()
+
+# 8.2.2 텍스트 자료 읽기
+
+# 파일 읽기 관련 함수
+ftest: Optional[TextIOWrapper] = None
+try:
+    # (1) read(): 전체 텍스트 자료 읽기
+    ftest = open(f'..{sep}data{sep}ftest.txt', 'r')
+    full_text: str = ftest.read()
+    print(full_text)
+    print(type(full_text), end='\n\n')
+
+    # (2) readlines(): 전체 텍스트 줄 단위 읽기
+    ftest = open(f'..{sep}data{sep}ftest.txt', 'r')
+    lines: list[str] = ftest.readlines()  # list 반환
+    print(lines)
+    print(type(lines))
+    print('문단 수:', len(lines), end='\n\n')
+
+    # (3) list -> 문장 추출
+    docs: list[str] = []  # 문장 저장
+    for line in lines:
+        print(line.strip())  # text만 출력
+        docs.append(line.strip())
+    print('docs =', docs, end='\n\n')
+
+    # (4) readline(): 한 줄 읽기
+    ftest = open(f'..{sep}data{sep}ftest.txt', 'r')
+    line: str = ftest.readline()  # 한 줄 읽기
+    print('line =', line)
+    print(type(line), end='\n\n')
+except Exception as e:
+    print('Error 발생:', e)
+finally:
+    # 파일 객체 닫기
+    ftest.close()
